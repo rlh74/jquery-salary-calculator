c4936f20ef60b8b2c08b79988cc6b5e610611be6
Ross Hutchens <rlhcxm@gmail.com>
2020-06-19 20:32:26 -0500

    Set up button to receive inputs from form and store in an object

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ Your project description goes here. What problem did you solve? How did you solv
 Create an application that records employee salaries and adds salaries up to report monthly costs.
 # Project Description
 [x] - create input form with inputs for first name, last name, ID, title, and salary
-[] - create a submit button
+[x] - create a submit button
 [] - set up button to receive inputs from form
-[] - store inputs 
+[] - store inputs
+[] - clear form after subtmit is clicked
 [] - set up function to calculate monthly cost
 [] - append monthly total to DOM
 [] - create a condition that checks if monthly cost exceeds $20,000
--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -1,6 +1,27 @@
 $(document).ready(onReady);
 console.log('js');
 
-function onReady(){
-    console.log('jq');
+const employeeData = [];
+
+function addEmployeeData(employeeFirstName, employeeLastName, employeeIdNumber, employeeJobTitle, employeeAnnualSalary){
+    let object = {employeeFirstName,  employeeLastName, employeeIdNumber, employeeJobTitle, employeeAnnualSalary};
+    employeeData.push(object);
 }
+
+function grabFormInput(){
+    event.preventDefault();
+    console.log('button has been clicked');
+    let employeeFirstName = $('#employeeFirstName').val();
+    let employeeLastName = $('#employeeLastName').val();
+    let employeeIdNumber = $('#employeeIdNumber').val();
+    let employeeJobTitle = $('#employeeJobTitle').val();
+    let employeeAnnualSalary = $('#employeeAnnualSalary').val();
+    addEmployeeData(employeeFirstName, employeeLastName, employeeIdNumber, employeeJobTitle, employeeAnnualSalary);
+    console.log('Array is now: ', employeeData);
+}
+
+function onReady(){
+    $('#submitEmployeeForm').on('click', grabFormInput)
+    // console.log('jq');
+}
+
