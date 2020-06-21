9214d3deef24fd070596aa7e63edce91b113cd6b
Ross Hutchens <rlhcxm@gmail.com>
2020-06-20 20:29:33 -0500

    Added comments to client.js and updated project outline in README

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -10,30 +10,51 @@ function addEmployeeData(employeeFirstName, employeeLastName, employeeIdNumber, 
 
 // prepare to step thru array to grab multiple salaries
 function calculateMonthlyTotal(employeeData){
+    // makes sure monthlyTotal is 0 before the function loops through array
     monthlyTotal = 0;
+    // loops through employeeData and creates a sum of employeeAnnualSalary property
     for (let i = 0 ; i<employeeData.length;i++){
         monthlyTotal += Number(employeeData[i].employeeAnnualSalary);
     }   console.log('monthly total is: ', monthlyTotal);
 };
 
+// append monthlyTotal to DOM
+function appendToDom(){
+    // empties monthlyTotal from HTML element before monthlyTotal is calculated and displayed
+    $('#monthlyTotal').empty();    
+    // console.log('we bout to append!'); tested if function ran successfully from grabFromInput function
+    // Appends total to DOM
+    $('#monthlyTotal').append(`Monthly total is: $`, monthlyTotal);
+    //$('#employeeTable').empty();
+};
+
+// grabs inputs from form input fields
 function grabFormInput(){
+    // prevent page from re-loading when button is clicked
     event.preventDefault();
-    console.log('button has been clicked');
+    // console.log('button has been clicked'); click test
+    // grabs user input
     let employeeFirstName = $('#employeeFirstName').val();
     let employeeLastName = $('#employeeLastName').val();
     let employeeIdNumber = $('#employeeIdNumber').val();
     let employeeJobTitle = $('#employeeJobTitle').val();
     let employeeAnnualSalary = $('#employeeAnnualSalary').val();
+    // sends input to addEmployeeData function
     addEmployeeData(employeeFirstName, employeeLastName, employeeIdNumber, employeeJobTitle, employeeAnnualSalary);
+    // clears input fields after user submission
     $('#employeeFirstName').val('');
     $('#employeeLastName').val('');
     $('#employeeIdNumber').val('');
     $('#employeeJobTitle').val('');
     $('#employeeAnnualSalary').val('');
-    console.log('Array is now: ', employeeData);
+    // console.log('Array is now: ', employeeData); used to test if inputs were added to global array
+    // calculates monthly total
     calculateMonthlyTotal(employeeData);
+    // begins append to DOM function
+    appendToDom();
 }
 
+// runs when page has loaded
 function onReady(){
     $('#submitEmployeeForm').on('click', grabFormInput)
 }
